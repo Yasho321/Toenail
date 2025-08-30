f405700f458c38751765aa3d9ca19dd49fbd8969
Yasho321 <yashovardhans321@gmail.com>
2025-08-30 14:21:43 +0530

    Completed basic auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import authRoutes from './routes/user.routes.js'
 
 import mongoose from "mongoose";
 
@@ -30,7 +31,7 @@ app.use(cors({
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use("/api/v1/auth", authRoutes)
 
 
 
