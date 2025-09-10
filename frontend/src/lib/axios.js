fa29197b16056fce0625ec4cf93a2b55348aa0b4
Yasho321 <yashovardhans321@gmail.com>
2025-09-10 08:12:56 +0530

    Fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -7,12 +7,12 @@ export const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-axiosInstance.interceptors.request.use((config) => {
+// axiosInstance.interceptors.request.use((config) => {
  
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+//   const token = localStorage.getItem('token');
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
   
-  return config;
-});
+//   return config;
+// });
