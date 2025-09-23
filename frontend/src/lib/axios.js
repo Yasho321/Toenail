c239bcd1c6d43b0a202e4b8fdeb71ee7100d3b50
Yasho321 <yashovardhans321@gmail.com>
2025-09-23 21:01:12 +0530

    Axios update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: 'https://toenail-6zcz.onrender.com/api/v1',
   withCredentials: true,
 });
 
